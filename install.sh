3595f0b002f174566acfca482c5d414e4d1db0a1
Tristan Scott <tristanascott123@gmail.com>
2022-07-13 01:05:03 -0500

    Initial Commit

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+THEME_DIR='usr/share/grub/themes'
+
+if [[ ! -d "${THEME_DIR}/ortho_theme" ]]; then
+	mkdir -p "${THEME_DIR}/ortho_theme"
+	cp -a ./theme/* "${THEME_DIR}/ortho_theme"
+fi
+
+sed -i '/GRUB_TIMEOUT_STYLE=/d' /etc/default/grub
+echo 'GRUB_TIMEOUT_STYLE="menu"' >> /etc/default/grub
+
+sed -i '/GRUB_TIMEOUT=/d' /etc/default/grub
+echo 'GRUB_TIMEOUT="10"' >> /etc/default/grub
+
+sed -i '/GRUB_THEME=/d' /etc/default/grub
+echo "GRUB_THEME=\"${THEME_DIR}/ortho_theme/theme.txt\"" >> /etc/default/grub
+
+grub-mkconfig -o /boot/grub/grub.cfg
